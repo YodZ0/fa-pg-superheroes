e4665bc38cbe1ee71495e092f1b89805b1946fd1
YodZ0 <timur.g00gle@gmail.com>
2025-08-07 22:32:16 +0500

    feat: create list+create use_cases plugs

---
+++ src/apps/superheroes/use_cases/create.py
@@ -0,0 +1,9 @@
+from typing import Protocol
+
+
+class CreateSuperheroUseCaseProtocol(Protocol):
+    pass
+
+
+class CreateSuperheroUseCaseImpl:
+    pass
---
+++ src/apps/superheroes/use_cases/list.py
@@ -0,0 +1,9 @@
+from typing import Protocol
+
+
+class ListSuperheroesUseCaseProtocol(Protocol):
+    pass
+
+
+class ListSuperheroesUseCaseImpl:
+    pass
