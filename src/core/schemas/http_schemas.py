631ad1bb2c3af2f01f5a817ea1b2278a87f99dc3
YodZ0 <timur.g00gle@gmail.com>
2025-08-07 22:29:10 +0500

    feat: create http schemas

---
+++ src/core/schemas/http_schemas.py
@@ -0,0 +1,33 @@
+from pydantic import AliasGenerator, BaseModel, ConfigDict
+from pydantic.alias_generators import to_camel
+
+
+class RequestSchema(BaseModel):
+    """
+    Request API schema.
+    """
+
+    model_config = ConfigDict(
+        alias_generator=AliasGenerator(
+            validation_alias=to_camel,
+        )
+    )
+
+
+class ResponseSchema(BaseModel):
+    """
+    Response API schema.
+    """
+
+    model_config = ConfigDict(
+        alias_generator=AliasGenerator(
+            serialization_alias=to_camel,
+        )
+    )
+
+
+class OKResponseSchema(BaseModel):
+    """
+    Default success response schema.
+    """
+    status: str = "OK"
