d88c004892b531978021c6c67e98fadcb8af6d97
YodZ0 <timur.g00gle@gmail.com>
2025-08-09 03:36:31 +0500

    feat: create db+http filter heroes exceptions

--- src/apps/superheroes/exceptions.py
+++ src/apps/superheroes/exceptions.py
@@ -1,4 +1,4 @@
-from typing import Any, TypeVar
+from typing import Any, TypeVar, Dict, Optional
 from fastapi import HTTPException, status
 
 from src.core.models.base import Base
@@ -13,11 +13,26 @@ class HeroNotFoundException(HTTPException):
     def __init__(
         self,
         hero_name: str,
-        headers: dict[str, Any] | None = None,
+        headers: Optional[Dict[str, Any]] = None,
     ):
         super().__init__(
             status_code=status.HTTP_404_NOT_FOUND,
             detail=f"Unable to find hero with name '{hero_name}'.",
+            headers=headers,
+        )
+
+
+class FilteredHeroesNotFoundException(HTTPException):
+    """Heroes with passed filters not found HTTP exception."""
+
+    def __init__(
+        self,
+        filters: Optional[Dict[str, Any]] = None,
+        headers: Optional[Dict[str, Any]] = None,
+    ):
+        super().__init__(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Unable to find heroes with filters '{filters}'.",
             headers=headers,
         )
 
@@ -38,3 +53,21 @@ class DBHeroNotFoundException(Exception):
         super().__init__(message, *args)
         self.model_type = model_type
         self.name = name
+
+
+class DBFilteredHeroesNotFoundException(Exception):
+    """
+    Models with passed filters not found exception
+    """
+
+    def __init__(
+        self,
+        model_type: ModelType,
+        filters: Dict[str, Any],
+        *args: object,
+    ) -> None:
+        model_name = model_type.__name__
+        message = f"Models '{model_name}' with filters '{filters}' not found."
+        super().__init__(message, *args)
+        self.model_type = model_type
+        self.filters = filters
