c571e85ac92e4d86a43b30f2a4ec1f39bc35b903
YodZ0 <timur.g00gle@gmail.com>
2025-08-09 03:37:03 +0500

    feat: create ListSuperheroesUseCase impl

--- src/apps/superheroes/use_cases/list.py
+++ src/apps/superheroes/use_cases/list.py
@@ -1,9 +1,33 @@
-from typing import Protocol
+from typing import Protocol, List
+
+from ..services.superheroes import SuperheroesServiceProtocol
+from ..schemas.superheroes import SuperheroReadSchema, SuperheroQueryFilterSchema
+from ..exceptions import HeroNotFoundException, FilteredHeroesNotFoundException
 
 
 class ListSuperheroesUseCaseProtocol(Protocol):
-    pass
+    async def execute(
+        self,
+        filters: SuperheroQueryFilterSchema,
+    ) -> List[SuperheroReadSchema]: ...
 
 
 class ListSuperheroesUseCaseImpl:
-    pass
+    def __init__(self, superheroes_service: SuperheroesServiceProtocol) -> None:
+        self.superheroes_service = superheroes_service
+
+    async def execute(
+        self,
+        filters: SuperheroQueryFilterSchema,
+    ) -> List[SuperheroReadSchema]:
+        if filters.name:
+            superhero = await self.superheroes_service.find_hero_by_name(filters.name)
+            if superhero is None:
+                raise HeroNotFoundException(hero_name=filters.name)
+            return [superhero]
+
+        superheroes = await self.superheroes_service.filter_heroes(filters=filters)
+        if superheroes is None:
+            raise FilteredHeroesNotFoundException(filters=filters.model_dump(exclude_none=True))
+
+        return superheroes
