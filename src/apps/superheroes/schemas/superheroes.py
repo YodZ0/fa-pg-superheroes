56bfa07028a062e211eabcebfa2905df2026b8a6
YodZ0 <timur.g00gle@gmail.com>
2025-08-09 03:37:37 +0500

    feat: create SuperheroQueryFilterSchema schema for query params

--- src/apps/superheroes/schemas/superheroes.py
+++ src/apps/superheroes/schemas/superheroes.py
@@ -1,3 +1,6 @@
+from typing import Optional
+from pydantic import BaseModel, Field
+
 from src.core.schemas.http_schemas import ResponseSchema
 from src.core.schemas.db_schemas import CreateBaseModel, UpdateBaseModel
 
@@ -25,3 +28,35 @@ class SuperheroCreateSchema(CreateBaseModel):
 
 class SuperheroUpdateSchema(UpdateBaseModel):
     pass
+
+
+class SuperheroQueryFilterSchema(BaseModel):
+    name: Optional[str] = Field(None, description="Exact match for name")
+
+    intelligence: Optional[int] = Field(
+        None, ge=0, description="Exact match for intelligence"
+    )
+    intelligence_ge: Optional[int] = Field(None, ge=0)
+    intelligence_le: Optional[int] = Field(None, ge=0)
+
+    strength: Optional[int] = Field(None, ge=0, description="Exact match for strength")
+    strength_ge: Optional[int] = Field(None, ge=0)
+    strength_le: Optional[int] = Field(None, ge=0)
+
+    speed: Optional[int] = Field(None, ge=0, description="Exact match for speed")
+    speed_ge: Optional[int] = Field(None, ge=0)
+    speed_le: Optional[int] = Field(None, ge=0)
+
+    durability: Optional[int] = Field(
+        None, ge=0, description="Exact match for durability"
+    )
+    durability_ge: Optional[int] = Field(None, ge=0)
+    durability_le: Optional[int] = Field(None, ge=0)
+
+    power: Optional[int] = Field(None, ge=0, description="Exact match for power")
+    power_ge: Optional[int] = Field(None, ge=0)
+    power_le: Optional[int] = Field(None, ge=0)
+
+    combat: Optional[int] = Field(None, ge=0, description="Exact match for combat")
+    combat_ge: Optional[int] = Field(None, ge=0)
+    combat_le: Optional[int] = Field(None, ge=0)
