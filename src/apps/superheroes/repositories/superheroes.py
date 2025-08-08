5624fab721fedbd79b1bdcf751a87059d64783be
YodZ0 <timur.g00gle@gmail.com>
2025-08-08 19:30:37 +0500

    feat: add get_by_name method

--- src/apps/superheroes/repositories/superheroes.py
+++ src/apps/superheroes/repositories/superheroes.py
@@ -1,3 +1,5 @@
+from sqlalchemy import select
+
 from src.core.models.superheroes import Superhero
 from src.core.repositories.db_repository import (
     DatabaseRepositoryProtocol,
@@ -8,6 +10,14 @@ from ..schemas.superheroes import (
     SuperheroCreateSchema,
     SuperheroUpdateSchema,
 )
+from ..exceptions import DBHeroNotFoundException
+
+_ConcreteBaseRepositoryIml = DatabaseRepositoryImpl[
+    Superhero,
+    SuperheroReadSchema,
+    SuperheroCreateSchema,
+    SuperheroUpdateSchema,
+]
 
 
 class SuperheroesRepositoryProtocol(
@@ -17,17 +27,18 @@ class SuperheroesRepositoryProtocol(
         SuperheroCreateSchema,
         SuperheroUpdateSchema,
     ],
-): ...
+):
+    async def get_by_name(self, name: str) -> SuperheroReadSchema: ...
 
 
-_ConcreteRepositoryBase = DatabaseRepositoryImpl[
-    Superhero,
-    SuperheroReadSchema,
-    SuperheroCreateSchema,
-    SuperheroUpdateSchema,
-]
-
-
-class SuperheroesRepositoryImpl(_ConcreteRepositoryBase):
+class SuperheroesRepositoryImpl(_ConcreteBaseRepositoryIml):
     model_type = Superhero
     read_schema_type = SuperheroReadSchema
+
+    async def get_by_name(self, name: str) -> SuperheroReadSchema:
+        async with self._session as s:
+            query = select(self.model_type).where(self.model_type.name == name)
+            model = (await s.execute(query)).scalar_one_or_none()
+            if model is None:
+                raise DBHeroNotFoundException(self.model_type, name)
+            return self.read_schema_type.model_validate(model, from_attributes=True)
